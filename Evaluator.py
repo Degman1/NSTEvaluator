8b4ff883e9b4c25fa22a7ddffb75a0c5d9caff43
bmhall17 <bmhall0826@gmail.com>
2024-11-27 14:18:16 -0500

    Correct Commented out lines in Evaluator.py

--- Evaluator.py
+++ Evaluator.py
@@ -377,14 +377,14 @@ class Evaluator:
             if model_dir == ".DS_Store":
                 continue
             stylized_folder_path = os.path.join(output, model_dir)
-            #artfid = Evaluator.artFidHandler(style, content, stylized_folder_path)
+            artfid = Evaluator.artFidHandler(style, content, stylized_folder_path)
             ssims = Evaluator.structuralSimilarity(content, stylized_folder_path)
             colorsims = Evaluator.colorSimilarity(style, stylized_folder_path)
             contentLoss, styleLoss = Evaluator.contentStyleLoss(content, style, stylized_folder_path)
             avg_times, load_time, unit = Evaluator.timePerformance(stylized_folder_path)
             ani_score = Evaluator.animationPerformance(np.mean(ssims), np.mean(colorsims), np.mean(contentLoss), np.mean(styleLoss), avg_times, load_time)
             results[model_dir] = {
-            #"ArtFID": artfid,
+            "ArtFID": artfid,
             "SSIM": np.mean(ssims),
             "ColorSim": np.mean(colorsims),
             "ContentLoss": np.mean(contentLoss),
@@ -396,11 +396,11 @@ class Evaluator:
         return results
     
 
-#content_path = sys.argv[1]
-#style_path = sys.argv[2]
-#output_path = sys.argv[3]
-content_path = "/Users/bmhall17/Desktop/UMass/Fall 2024/CS682 Neural Networks/cs682finalproject/NSTEvaluator/content_images"
-style_path = "/Users/bmhall17/Desktop/UMass/Fall 2024/CS682 Neural Networks/cs682finalproject/NSTEvaluator/style_images"
-output_path = "/Users/bmhall17/Downloads/output"
+content_path = sys.argv[1]
+style_path = sys.argv[2]
+output_path = sys.argv[3]
+#content_path = "/Users/bmhall17/Desktop/UMass/Fall 2024/CS682 Neural Networks/cs682finalproject/NSTEvaluator/content_images"
+#style_path = "/Users/bmhall17/Desktop/UMass/Fall 2024/CS682 Neural Networks/cs682finalproject/NSTEvaluator/style_images"
+#output_path = "/Users/bmhall17/Downloads/output"
 evaluation_results = Evaluator.evaluate(style_path, content_path, output_path)
 print(evaluation_results)
